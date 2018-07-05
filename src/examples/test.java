d40dafa03f9d1571eabeea5413972ce6472c1889
kwonhyucknae <nhk321@naver.com>
2018-07-06 03:02:50 +0900

    현재 날짜 기능 서블릿 추가

---
+++ src/examples/test.java
@@ -0,0 +1,29 @@
+package examples;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/test")
+public class test extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+    public test() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("test");
+	}
+
+
+	
+}
---
+++ src/time/Localtime.java
@@ -0,0 +1,43 @@
+package time;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/today")
+public class Localtime extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+    public Localtime() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		response.setContentType("text/html;charset=UTF-8"); 
+		PrintWriter out=response.getWriter();
+		LocalDateTime lt=LocalDateTime.now();
+		System.out.println(lt.getYear()+":"+lt.getMonthValue()+":"+lt.getDayOfMonth()+":"+lt.getHour()+":"+lt.getMinute());
+		
+		out.print("<a href=\"...\">메인화면</a>");
+		out.print("<br/>");
+		out.print("<br/>");
+		out.print("<br/>");
+		out.print("<br/>");
+		
+		out.print("<h2>"+lt.getYear()+":"+lt.getMonthValue()+":"+lt.getDayOfMonth()+":"+lt.getHour()+":"+lt.getMinute()+"</h2>");
+		
+	}
+
+
+}
